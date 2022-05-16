709a8356fdcb3d97d943e6f71678f9ab2d1c8d14
sanket <sammane228@gmail.com>
2022-05-16 23:18:22 +0530

    Crud todo

--- src/App.js
+++ src/App.js
@@ -1,64 +1,102 @@
-import React, { useState } from 'react'
-import Li from './component/Li'
-import './App.css'
+import React, { useState } from "react";
+import Li from "./component/Li";
+import "./App.css";
 
 const App = () => {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([
+    {
+      id: 1,
+      title: "Walk the dog",
+    },
+    {
+      id: 2,
+      title: "Finish Homework",
+    },
+  ]);
   const [inputList, setInputList] = useState("");
+  const [updateItemId, setUpdateItemId] = useState("");
 
   const handleChange = (e) => {
     setInputList(e.target.value);
-  }
-
-
+  };
 
   const addData = (e) => {
     e.preventDefault();
-      (inputList === "") ? 
-      alert("please fill the task before add") :
-      (setItems((olditems) => {
-        return [...olditems, inputList];
-      }))
+    if (!updateItemId) {
+      const enteredTodo = {
+        id: Math.random(),
+        title: inputList,
+      };
+      inputList === ""
+        ? alert("please fill the task before add")
+        : setItems((olditems) => {
+            return [...olditems, enteredTodo];
+          });
+    } else {
+      const updatedTodo = {
+        id: updateItemId,
+        title: inputList,
+      };
+
+      let newTodo = [...items];
+      let findUpdateIndex = newTodo.findIndex(
+        (item) => item.id == updateItemId
+      );
+      newTodo[findUpdateIndex] = updatedTodo;
+      setItems(newTodo);
+      setUpdateItemId("");
+    }
     setInputList("");
+  };
 
-  }
-
-  const deleteItems = (id) => {
+  const deleteItem = (id) => {
     setItems((olditems) => {
       return olditems.filter((item, index) => {
-        return index !== id
-      })
-    })
-  }
+        return item.id !== id;
+      });
+    });
+  };
+
+  const updateItem = (data) => {
+    setInputList(data.title);
+    setUpdateItemId(data.id);
+  };
 
   return (
     <div className="container">
       <div className="wrapper">
         <h1>Todo List</h1>
-        <form>
+        <form onSubmit={addData}>
           <div className="form-group">
             <input type="text" onChange={handleChange} value={inputList} />
           </div>
-          <div className="form-group">
-            <input type="submit" value="add todo" onClick={addData} />
-          </div>
+          {updateItemId ? (
+            <div className="form-group">
+              <input type="submit" value="Updated Todo" />
+            </div>
+          ) : (
+            <div className="form-group">
+              <input type="submit" value="Add todo" />
+            </div>
+          )}
         </form>
         <br />
         <ol className="todo_ul">
-          {
-            items.map((items, index) =>
-              <Li
-                key={index}
-                id={index}
-                delete={deleteItems}
-                text={items}
-              />)
-          }
+          {items.map((item, index) => (
+            <Li
+              key={item.id}
+              id={item.id}
+              index={index}
+              title={item.title}
+              delete={deleteItem}
+              text={items}
+              update={updateItem}
+            />
+          ))}
         </ol>
-
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/component/Li.js
+++ src/component/Li.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import './Li.css'
+import React from "react";
+import "./Li.css";
 function Li(props) {
-
-
   return (
     <li>
-      <p>{props.id + 1}</p>
-      <p>{props.text}</p>
-      <i className="fa fa-times" onClick={()=> {props.delete(props.id)}} />
+      <p style={{ marginRight: "20px" }}>{props.index + 1}</p>
+      <p>{props.title}</p>
+      <div
+        style={{ display: "flex", alignItems: "center", marginLeft: "auto" }}
+      >
+        <span style={{ marginRight: "10px" }}>
+          <i
+            className="fa fa-times"
+            onClick={() => {
+              props.delete(props.id);
+            }}
+          />
+        </span>
+        <span>
+          <i className="fa fa-pencil" onClick={() => props.update(props)} />
+        </span>
+      </div>
     </li>
-  )
+  );
 }
 
-export default Li
+export default Li;
